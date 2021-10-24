60fbbb0ec5e47ef2a7a4c0f67d9d012195839709
Lolo <loom4k.twitch@gmail.com>
2021-10-24 12:55:13 -0400

    ✨ Completed API

--- blockchain.py
+++ blockchain.py
@@ -6,8 +6,7 @@ from uuid import uuid4
 
 from textwrap import dedent
 
-from flask import Flask
-from flask.json import jsonify
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -112,11 +111,47 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new block"
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof
+    # The sender is "0" to signify that this node has mined a new coin
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_indentifier,
+        amount = 1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New block forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to the block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
